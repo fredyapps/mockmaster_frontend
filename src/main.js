74cafddae54da4a2c45e3d822a3865fe45c2c54a
frederick sallah <fredysallah@gmail.com>
2022-11-13 02:59:42 +0000

    migration cutover

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ const app = createApp(App)
 
 
 
-//app.config.globalProperties.api_url = 'https://tmmbackend.herokuapp.com';
+app.config.globalProperties.api_url = 'https://www.mymockmaster.com';
 
 //app.config.globalProperties.api_url = 'http://localhost:9000';
 
-app.config.globalProperties.api_url = 'http://ec2-184-73-1-181.compute-1.amazonaws.com:9000';
+//app.config.globalProperties.api_url = 'http://ec2-184-73-1-181.compute-1.amazonaws.com:9000';
 
 
 
